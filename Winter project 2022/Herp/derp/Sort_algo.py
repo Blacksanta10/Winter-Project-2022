1cae6a1f4cad395e111081190613bc9727626845
Demitrius Webb <101858569+Blacksanta10@users.noreply.github.com>
2022-12-18 23:51:54 -0500

    Add files via upload

---
+++ Winter project 2022/Herp/derp/Sort_algo.py
@@ -0,0 +1,30 @@
+# This page is the source to different sorting algorithms 
+
+def sort_bubble(a):  ##Bubble sort algorithm
+    for i in range(len(a)-1):
+        for j in range(len(a)-1):
+            if(a[j]>a[j+1]):
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
+    return(a)
+
+def linear_search(a,n):   ##linear search algorithm
+    for i in range(len(a)):
+        if a[i]==n:
+            return(i)
+    return('False')
+        
+def max_value(a,b):     ##gives max value in lists
+    if(a>b):
+        max=a
+    else:
+        max=b
+    return(max)
+
+def max_list(a):
+    max=a[0]
+    for i in range(1,len(a)):
+        if (a[i]>max):
+            max=a[i]
+    return(max)
